da362112d39b4a6ac51926dedfbf15bbbf12a1de
Nosmallero <diego_vasquez4@hotmail.com>
2022-05-19 21:46:54 -0500

    Creating DiegoVasquez Project

---
+++ diegovasquez/src/main/java/pe/edu/upc/diegovasquez/DiegovasquezApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.diegovasquez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiegovasquezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiegovasquezApplication.class, args);
+	}
+
+}
