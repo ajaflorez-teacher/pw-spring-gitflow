a12573294edfc1dbd1b6080a0eb7d98fba643686
Sergio Huaman-Cheng <sergiohuamanch15@gmail.com>
2022-05-19 21:49:22 -0500

    Creating SergioHuamanCheng Project

---
+++ sergiohuamancheng/src/main/java/pe/edu/upc/sergiohuamancheng/SergiohuamanchengApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.sergiohuamancheng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SergiohuamanchengApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SergiohuamanchengApplication.class, args);
+	}
+
+}
