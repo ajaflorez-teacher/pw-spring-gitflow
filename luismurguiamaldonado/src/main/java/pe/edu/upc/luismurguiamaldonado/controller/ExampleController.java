40abe20d9d76d5a76d3c14f90870eadea5a59753
lordgius <lg190799@hotmail.com>
2022-05-23 21:41:47 -0500

    adding luis

---
+++ luismurguiamaldonado/src/main/java/pe/edu/upc/luismurguiamaldonado/controller/ExampleController.java
@@ -0,0 +1,15 @@
+package pe.edu.upc.luismurguiamaldonado.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/ejemplo")
+public class ExampleController {
+	
+	@GetMapping
+	public String ejemplo() {
+		return "example";
+	}
+}
