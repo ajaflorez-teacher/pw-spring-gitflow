d540af84e288a767088efe046ab2e8effe970604
Brian Liñan Acosta <brunosanto123@hotmail.com>
2022-06-13 21:05:57 -0500

    Update with Sprint Security

---
+++ university/src/main/java/pe/edu/upc/university/controller/ExampleController.java
@@ -0,0 +1,20 @@
+package pe.edu.upc.university.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/ejemplo")
+public class ExampleController {
+	
+	@GetMapping	//	/ejemplo
+	public String ejemplo() {
+		return "example";
+	}
+	
+	@GetMapping("base")	//  /ejemplo/base
+	public String base() {
+		return "layout/base";
+	}
+}
